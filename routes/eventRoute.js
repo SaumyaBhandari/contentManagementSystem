aa6bdeca16058960eba98a2b439e2f494694bc18
amrit sharma <sharmaamrit.a5432@gmail.com>
2021-05-13 14:45:09 +0545

    fixed event issue

--- routes/eventRoute.js
+++ routes/eventRoute.js
@@ -3,29 +3,24 @@ const router = require('express').Router();
 const eventController = require('../controllers/eventController');
 const authController = require('../controllers/authController');
 
-router.use(authController.protect, authController.restrictTo('admin'));
+router.use(
+  authController.protect,
+  authController.restrictTo('admin', 'superadmin')
+);
 router
   .route('/')
   .get(eventController.getEvents)
-  .post(
-    authController.restrictTo('admin', 'superadmin'),
-    eventController.uploadCoverPhoto,
-    eventController.addEvent
-  );
+  .post(eventController.uploadCoverPhoto, eventController.addEvent);
 router
   .route('/:id')
   .get(eventController.getSingleEvent)
-  .delete(
-    authController.restrictTo('admin', 'superadmin'),
-    eventController.deleteEvent
-  );
+  .delete(eventController.deleteEvent);
 
 router.post(
   '/update/:id',
 
   eventController.uploadCoverPhoto,
 
-  authController.restrictTo('admin', 'superadmin'),
   eventController.updateEvent
 );
 
